2ce82a73545edee3c3048eb0d314cb911f4f2813
Sabuhi Ibrahimov <sabuhi@Sabuhis-MacBook-Pro.local>
2023-10-16 00:25:09 +0400

    backend ready

---
+++ frontend/src/types/club.ts
@@ -0,0 +1,17 @@
+export type ClubItem = {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  created_at: string;
+  updated_at: string;
+};
+
+export type Club = {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  created_at: string;
+  updated_at: string;
+};
