9ed3623f617078116a598da6982d7fdfb60d0cea
Sabuhi Ibrahimov <sabuhi@Sabuhis-MacBook-Pro.local>
2023-10-18 23:45:13 +0400

    configure backend & nginx

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -160,7 +160,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/django-static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join("media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
